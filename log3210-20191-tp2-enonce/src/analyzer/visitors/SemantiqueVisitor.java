5920a088a5384da190da65cc8b2d7b25dd1dff20
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2019-02-04 08:42:21 -0500

    Ajout du code TP2

---
+++ log3210-20191-tp2-enonce/src/analyzer/visitors/SemantiqueVisitor.java
@@ -0,0 +1,235 @@
+package analyzer.visitors;
+
+import analyzer.SemantiqueError;
+import analyzer.ast.*;
+
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+
+/**
+ * Created: 19-01-10
+ * Last Changed: 19-01-25
+ * Author: Félix Brunet
+ *
+ * Description: Ce visiteur explorer l'AST est renvois des erreur lorqu'une erreur sémantique est détecté.
+ */
+
+public class SemantiqueVisitor implements ParserVisitor {
+
+    private final PrintWriter m_writer;
+
+    public HashMap<String, VarType> SymbolTable = new HashMap<>();
+
+    public SemantiqueVisitor(PrintWriter writer) {
+        m_writer = writer;
+    }
+
+    /*
+    Le Visiteur doit lancer des erreurs lorsqu'un situation arrive.
+
+    pour vous aider, voici le code a utilisé pour lancer les erreurs
+
+    //utilisation d'identifiant non défini
+    throw new SemantiqueError("Invalid use of undefined Identifier " + node.getValue());
+
+    //utilisation de nombre dans la condition d'un if ou d'un while
+    throw new SemantiqueError("Invalid type in condition");
+
+    //assignation d'une valeur a une variable qui a déjà recu une valeur d'un autre type
+    ex : a = 1; a = true;
+    throw new SemantiqueError("Invalid type in assignment");
+
+    //expression invalide : (note, le code qui l'utilise est déjà fournis)
+    throw new SemantiqueError("Invalid type in expression got " + type.toString() + " was expecting " + expectedType);
+
+     */
+
+    @Override
+    public Object visit(SimpleNode node, Object data) {
+        return data;
+    }
+
+    @Override
+    public Object visit(ASTProgram node, Object data)  {
+        node.childrenAccept(this, data);
+        m_writer.print("all good");
+        return null;
+    }
+
+    /*
+    Doit enregistrer les variables avec leur type dans la table symbolique.
+     */
+    @Override
+    public Object visit(ASTDeclaration node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTBlock node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+
+    @Override
+    public Object visit(ASTStmt node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    /*
+    les structures conditionnelle doivent vérifier que leur expression de condition est de type booléenne
+     */
+    @Override
+    public Object visit(ASTIfStmt node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTWhileStmt node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    /*
+    On doit vérifier que le type de la variable est compatible avec celui de l'expression.
+     */
+    @Override
+    public Object visit(ASTAssignStmt node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTExpr node, Object data){
+        //Il est normal que tous les noeuds jusqu'à expr retourne un type.
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTCompExpr node, Object data) {
+        /*attention, ce noeud est plus complexe que les autres.
+        si il n'a qu'un seul enfant, le noeud a pour type le type de son enfant.
+
+        si il a plus d'un enfant, alors ils s'agit d'une comparaison. il a donc pour type "Bool".
+
+        de plus, il n'est pas acceptable de faire des comparaisons de booleen avec les opérateur < > <= >=.
+        les opérateurs == et != peuvent être utilisé pour les nombres et les booléens, mais il faut que le type soit le même
+        des deux côté de l'égalité/l'inégalité.
+        */
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    /*
+    opérateur binaire
+    si il n'y a qu'un enfant, aucune vérification à faire.
+    par exemple, un AddExpr peut retourné le type "Bool" à condition de n'avoir qu'un seul enfant.
+     */
+    @Override
+    public Object visit(ASTAddExpr node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTMulExpr node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTBoolExpr node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    /*
+    opérateur unaire
+    les opérateur unaire ont toujours un seul enfant.
+
+    Cependant, ASTNotExpr et ASTUnaExpr ont la fonction "getOps()" qui retourne un vecteur contenant l'image (représentation str)
+    de chaque token associé au noeud.
+
+    Il est utile de vérifier la longueur de ce vecteur pour savoir si une opérande est présente.
+
+    si il n'y a pas d'opérande, ne rien faire.
+    si il y a une (ou plus) opérande, ils faut vérifier le type.
+
+    */
+    @Override
+    public Object visit(ASTNotExpr node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTUnaExpr node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    /*
+    les noeud ASTIdentifier aillant comme parent "GenValue" doivent vérifier leur type.
+
+    Ont peut envoyé une information a un enfant avec le 2e paramètre de jjtAccept ou childrenAccept.
+     */
+    @Override
+    public Object visit(ASTGenValue node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+
+    @Override
+    public Object visit(ASTBoolValue node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTIdentifier node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTIntValue node, Object data) {
+        return null;
+    }
+
+
+    //des outils pour vous simplifier la vie et vous enligner dans le travail
+    public enum VarType {
+        Bool,
+        Number
+    }
+
+    private boolean estCompatible(VarType a, VarType b) {
+        return a == b;
+    }
+
+    private class DataStruct {
+        public VarType type;
+
+        public DataStruct() {}
+
+        public DataStruct(VarType p_type){
+            type = p_type;
+        }
+
+        public void checkType(VarType expectedType) {
+            if(!estCompatible(type, expectedType)) {
+                throw new SemantiqueError("Invalid type in expression got " + type.toString() + " was expecting " + expectedType);
+            }
+        }
+
+        public void checkType(DataStruct d, VarType expectedType) {
+            if(!estCompatible(type, expectedType) || !estCompatible(d.type, expectedType)) {
+                throw new SemantiqueError("Invalid type in expression got " + type.toString() + " and " + d.type.toString() + " was expecting " + expectedType);
+            }
+        }
+    }
+}
